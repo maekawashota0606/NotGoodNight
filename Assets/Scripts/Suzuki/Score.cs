a0ffca7d2ae1074dcc1bbdb4cea1c79d8cfbfbee
Masahito <masagozira4999@gmail.com>
2022-05-13 14:05:36 +0900

    スコア表示の微調整

--- Assets/Scripts/Suzuki/Score.cs
+++ Assets/Scripts/Suzuki/Score.cs
@@ -11,7 +11,7 @@ public class Score : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = "Score" + score.ToString();
+        textComponent.text = "Score" + score.ToString("d7");
     }
 
     // Update is called once per frame
@@ -25,7 +25,7 @@ public class Score : MonoBehaviour
 
     public void AddScore()
     {
-        score += 100;
-        textComponent.text = "Score" + score.ToString();
+        score += 1000;
+        textComponent.text = "Score" + score.ToString("d7");
     }
 }
