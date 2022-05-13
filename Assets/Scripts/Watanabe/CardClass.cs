22090f422d0ee6140287be9079ce97fc2b4873d6
Violetkuroinu <21c1020055wt@edu.tech.ac.jp>
2022-05-13 18:26:24 +0900

    カードに効果とコストの実装。クラス設計

--- Assets/Scripts/Watanabe/CardClass.cs
+++ Assets/Scripts/Watanabe/CardClass.cs
@@ -1,19 +1,59 @@
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-[System.Serializable]
-public Ability
+/*[System.Serializable]
+#include <string>
+using namespace std;
+public class Card
 {
-    //発動タイミング（ターン開始時）
-    public string AbilityName;
-    //スキルタイプ（カードを引く）
-    public Target Target;
-    //発動対象（隕石）
-    public string TargetCondition;
-    //発動条件（Xの値に入る数値）
-    public int TargetOption;
-    //効果点数（ドロー枚数）
-    public int AbilityAmount; 
-}
+    private: 
+	    string Cardclass; //メンバ変数
+
+    public:
+    メンバ関数のプロトタイプ宣言
+	    void print1(AttackAbillity);     //攻撃呪文
+        void print2(CoveringAbillity); //援護呪文
+        void print3(Cost);                   //カードコスト
+        void print4(CardID);               //カードID
+};
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*public interface Card
+{
+    カードクラス
+    攻撃呪文
+    private AttackAbillity;
+    援護呪文
+    private CoveringAbillity;
+    コスト
+    private Cost;
+    カードID
+    private CardID;
+}*/
+
+/* //発動タイミング（ターカードン開始時）
+ public string AbilityName;
+ スキルタイプ（カードを引く）
+ public Target Target;
+ 発動対象（隕石）
+ public string TargetCondition;
+ 発動条件（Xの値に入る数値）
+ public int TargetOption;
+ 効果点数（ドロー枚数）
+ public int AbilityAmount; 
+ */
+
