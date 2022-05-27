216d05abff3e2d57b6dcdc67de051948952fc8ac
Violetkuroinu <21c1020055wt@edu.tech.ac.jp>
2022-05-27 18:34:22 +0900

    ドロー効果の実装

---
+++ Assets/Scripts/Watanabe/Scripts/Card.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public class Card : MonoBehaviour
+{
+    public draw drawGet;
+    void Start()
+    {
+        drawGet = GetComponent.Find<Button>;
+        drawmain = drawGet.GetComponent;
+    }
+    void Update()
+    {
+        int drawat = drawGet.Draw;
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            drawGet(3);
+        }
+    }
+}*/
