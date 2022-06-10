2428c6e1f927cf8a77040db233a0e04147a1793d
Violetkuroinu <21c1020055wt@edu.tech.ac.jp>
2022-06-10 14:39:04 +0900

    ぼかし修正

--- Assets/Scripts/Watanabe/CE_DrawCard.cs
+++ Assets/Scripts/Watanabe/CE_DrawCard.cs
@@ -6,21 +6,21 @@
 
 //internal class CardEffective : CardEffect
 //{
-//	public int nDraw { get; private set; }
-//	public CardEffective(Card Owner, int nDraw) { this.Owner = Owner; this.nDraw = nDraw; }
-//	#region CardEffect
-//	public int Name { get { return "DrawCard"; } }
-//	public int EffectText { get { return "カードを " + nDraw + " 枚引く"; } }
-//	public Card Owner { get; private set; }
-//	public void Affect(GameContextForCardEffect IGC)
-//	{
-//		for (int i = 0; i < nDraw; ++i)
-//		{
-//			var C = IGC.DrawFromDeck();
-//			Console.WriteLine(C.Name + " をドロー");
-//			IGC.AddToHand(C);
-//		}
-//	}
-//	#endregion
-//	}
+//    public int nDraw { get; private set; }
+//    public CardEffective(Card Owner, int nDraw) { this.Owner = Owner; this.nDraw = nDraw; }
+//    #region CardEffect
+//    public int Name { get { return "DrawCard"; } }
+//    public int EffectText { get { return "カードを " + nDraw + " 枚引く"; } }
+//    public Card Owner { get; private set; }
+//    public void Affect(GameContextForCardEffect IGC)
+//    {
+//        for (int i = 0; i < nDraw; ++i)
+//        {
+//            var C = IGC.DrawFromDeck();
+//            Console.WriteLine(C.Name + " をドロー");
+//            IGC.AddToHand(C);
+//        }
+//    }
+//    #endregion
+//}
 
