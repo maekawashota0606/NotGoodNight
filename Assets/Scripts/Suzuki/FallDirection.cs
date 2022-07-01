06e2befc778604e29995fd0d55941e0809610234
Masahito <masagozira4999@gmail.com>
2022-07-01 18:09:59 +0900

    隕石落下の演出できた

--- Assets/Scripts/Suzuki/FallDirection.cs
+++ Assets/Scripts/Suzuki/FallDirection.cs
@@ -6,12 +6,15 @@ public class FallDirection : MonoBehaviour
 {
     public List<GameObject> meteors = new List<GameObject>();
 
-    private float Times;
+    private float Times = 0f;
+    public float Timev = 1f;
+
+    public bool DoNextTurn = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,11 +22,8 @@ public class FallDirection : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.N))
         {
+
             Transform Trans = this.transform;
-
-            Times = Time.deltaTime;
-
-
             Vector3 pos = Trans.position;
             pos.z -= 0.1f;
             Trans.position = pos;
@@ -32,6 +32,28 @@ public class FallDirection : MonoBehaviour
             {
                 this.transform.position();
             }*/
+
         }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            DoNextTurn = true;
+        }
+
+
+        if (DoNextTurn)
+        {
+            Times += Time.deltaTime;
+
+            this.transform.position += new Vector3(0, 0, -1.0f * Time.deltaTime);
+
+            if (Times >= Timev)
+            {
+                Times = 0;
+                DoNextTurn = false;
+            }
+        }
+
+
     }
 }
