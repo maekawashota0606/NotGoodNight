0a0efaa7f5ed6d3b602cdff77e3bb2c36b2b38e1
Masahito <masagozira4999@gmail.com>
2022-07-08 15:11:51 +0900

    詳細枠のUIの調整

--- Assets/Scripts/Suzuki/GameClearSceneChange.cs
+++ Assets/Scripts/Suzuki/GameClearSceneChange.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameClearSceneChange : MonoBehaviour
 {
+    //ゲームクリアシーンからタイトルシーンへの遷移
     public void SwitchScene_Title()
     {
         SceneManager.LoadScene("Title");
